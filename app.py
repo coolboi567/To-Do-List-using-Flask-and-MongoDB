239f5b72a3505b617af0b74bb3eb765ef4d6cb1b
kishorAB <kishorab456@gmail.com>
2022-12-28 15:27:20 +0530

    new

--- app.py
+++ app.py
@@ -121,7 +121,7 @@ def about():
 
 if __name__ == "__main__":
 	env = os.environ.get('FLASK_ENV', 'development')
-	port = int(os.environ.get('PORT', 80))
+	port = int(os.environ.get('PORT',8000))
 	debug = False if env == 'production' else True
-	app.run(host='0.0.0.0',port=80)
+	app.run(host='0.0.0.0',port=8000)
 	# Careful with the debug mode..
